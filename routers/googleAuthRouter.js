891eeac7e12623a2a07ee471a945fd92f49b2994
naisannovel <naisannovel@gmail.com>
2022-05-26 21:22:52 +0600

    update

--- routers/googleAuthRouter.js
+++ routers/googleAuthRouter.js
@@ -6,8 +6,7 @@ require('../config/googleAuthConfig');
 router.get('/google',passport.authenticate('google',{scope:['profile','email']}))
 
 router.get('/google/callback',passport.authenticate('google',{ session: false }), (req,res)=>{
-    res.cookie('token', req.user)
-    res.redirect('http://localhost:3000/')
+    res.redirect(`http://localhost:3000/google-oauth/${req?.user}`); 
 })
 
 module.exports = router;
