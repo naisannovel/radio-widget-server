108f4569a435fcf65c9c57f79d6d58885385c6dd
naisannovel <naisannovel@gmail.com>
2022-05-27 00:13:19 +0600

    update

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next) =>{
-    console.log(token);
+    let token = req.header('Authorization');
     if(!token) return res.status(401).send('access denied, no token provide');
     token = token.split(' ')[1].trim();
 
